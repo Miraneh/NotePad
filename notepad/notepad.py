9df5c9ec77d23cf4daeb50451714e0fc70f2f236
Miraneh <najafi.mehraneh@gmail.com>
2021-03-17 01:07:05 +0330

    basic stuff adding to start

---
+++ notepad/notepad.py
@@ -0,0 +1,12 @@
+import tkinter
+import os
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+class NotePad:
+    __root = Tk()
+
+    __width = 300
+    __Height = 300
+    __thisTextArea = Text
